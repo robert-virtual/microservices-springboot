26e67448963b8d910fff5b577a03ef2208e2ba14
robert-virtual <robertocastill63@gmail.com>
2023-04-03 16:13:12 -0600

    circuit breaker

---
+++ apigateway/src/main/java/org/example/apigateway/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.example.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity httpSecurity
+    ) {
+        httpSecurity
+                .authorizeExchange(
+                        exchange -> exchange.pathMatchers("/eureka/*")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return httpSecurity.build();
+    }
+}
--- orders-service/src/main/java/org/example/orderservice/OrderController.java
+++ orders-service/src/main/java/org/example/orderservice/OrderController.java
@@ -1,7 +1,11 @@
 package org.example.orderservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -15,7 +19,11 @@ public class OrderController{
         return orderService.findAll();
     }
     @PostMapping("create")
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
     public Order create(@RequestBody Order order) throws Exception {
        return orderService.save(order);
+    }
+    public Order fallbackMethod(Order order, Exception e){
+        throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE,"Ups something went wrong");
     }
 }
