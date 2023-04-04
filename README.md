6af5eaafc9a014908b357fc274c4bf3e0415311f
robert-virtual <robertocastill63@gmail.com>
2023-04-04 09:31:14 -0600

    readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Spring boot microservices java 11
+
+## Main used libraries
+
+- Spring cloud starter gateway
+
+> Provide a central point to access and secure microservices
+
+- Spring cloud starter circuit-breaker resilience4j
+
+> close connection to a service when it does not response back
+
+- Spring cloud starter netflix eureka server
+
+> used to help services find one another
+
+- Spring cloud starter netflix eureka client
+
+> Communicate with eureka server
+
+- Spring boot starter webflux
+
+> make http requests from service to service
+
+- Keycloak (it is not a project dependency but another service that provides authorization)
--- orders-service/src/main/java/org/example/orderservice/OrderController.java
+++ orders-service/src/main/java/org/example/orderservice/OrderController.java
@@ -1,6 +1,8 @@
 package org.example.orderservice;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("orders")
@@ -18,12 +21,31 @@ public class OrderController{
     public List<Order> all(){
         return orderService.findAll();
     }
+    @PostMapping("create/retry")
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory") // retry 3 times and wait 5s (configured at application.yml)
+    public Order createRetry(@RequestBody Order order) throws Exception {
+        return orderService.save(order);
+    }
     @PostMapping("create")
     @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
-    public Order create(@RequestBody Order order) throws Exception {
-       return orderService.save(order);
+    public Order createSync(@RequestBody Order order) throws Exception {
+        return orderService.save(order);
     }
-    public Order fallbackMethod(Order order, Exception e){
+    @PostMapping("create/async")
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory") // waits 3 seconds (configured at application.yml) otherwise throws a timeout exception
+    public CompletableFuture<Order> createAsync(@RequestBody Order order) {
+        // runs in another thread
+       return CompletableFuture.supplyAsync(()-> {
+           try {
+               return orderService.save(order);
+           } catch (Exception e) {
+               throw new RuntimeException(e);
+           }
+       });
+    }
+    public CompletableFuture<Order> fallbackMethod(Order order, Exception e){
         throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE,"Ups something went wrong");
     }
 }
